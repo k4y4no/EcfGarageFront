bbbcde77e5f09c58d4e4b5c0e00d434a06df29fe
Thraem <k4y4no@gmail.com>
2023-07-21 04:50:47 +0200

    apiservice

---
+++ src/app/_services/api.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of, tap, map } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  urlProd: string = 'https://garagerestapi-a1ad312e44d1.herokuapp.com/api/'
+  urlLocal: string = 'http://localhost:8000/api/'
+
+  getList(url: string): Observable<[]> {
+
+    return this.http.get<[]>(`${this.urlLocal}${url}` , {
+      headers: new HttpHeaders({
+        'Accept':  'application/ld+json'
+      })
+    })
+  }
+
+  getById(url: string, id: string):  Observable<any> {
+    return this.http.get<[]>(`https://garagerestapi-a1ad312e44d1.herokuapp.com/api/${url}/${id}`, {
+      headers: new HttpHeaders({
+        'Accept':  'application/ld+json'
+      })
+    })
+  }
+
+}
