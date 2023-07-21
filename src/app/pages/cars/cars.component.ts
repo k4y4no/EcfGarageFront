7c7561ddbc41fbdef548ce675a7a8170ea8525d4
Thraem <k4y4no@gmail.com>
2023-07-21 04:47:07 +0200

    basic pages

---
+++ src/app/pages/cars/cars.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ecf-cars',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.scss']
+})
+export class CarsComponent {
+
+}
