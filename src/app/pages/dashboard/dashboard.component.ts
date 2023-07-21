2fb7f58b690516d2179aaf3e746a6be6b63f4dbc
Thraem <k4y4no@gmail.com>
2023-07-21 05:49:07 +0200

    dashboard basics

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormComponent } from 'src/app/spareParts/form/form.component';
+import { ListItemsComponent } from 'src/app/spareParts/list-items/list-items.component';
+import { Observable, of } from 'rxjs';
+
 
 @Component({
   selector: 'ecf-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ListItemsComponent, 
+    FormComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  panelRequested$!: Observable<string>;
+  
 
+
+
+  getThisPanel(value: string) {
+    this.panelRequested$ = of(value);
+  }
 }
