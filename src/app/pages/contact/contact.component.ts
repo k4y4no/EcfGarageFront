3dbda205ef33fd8342db60f19a8ff3b8c217d1ec
Thraem <k4y4no@gmail.com>
2023-07-21 05:47:08 +0200

    home&contact

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormComponent } from 'src/app/spareParts/form/form.component';
 
 @Component({
   selector: 'ecf-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormComponent
+  ],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListItemsComponent } from 'src/app/spareParts/list-items/list-items.component';
+import { CarouselComponent } from 'src/app/spareParts/carousel/carousel.component';
 
 @Component({
   selector: 'ecf-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ListItemsComponent,
+    CarouselComponent
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
